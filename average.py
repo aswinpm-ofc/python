ace0dfe70be5f4b913a0725f123abc5d30c90200
aswinpm-ofc <147654197+aswinpm-ofc@users.noreply.github.com>
2023-10-19 16:00:21 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,8 @@
+print("enter three nuhumbers which you want the average of the sum")
+a=int(input())
+b=int(input())
+c=int(input())
+AVERAGE=(a+b+c)/3
+some=a+b+c
+print(" THE SUM OF THE THREE NUMBERS=",some)
+print("average OF THE SUM OF THE THREE NUMBERS=",AVERAGE)
